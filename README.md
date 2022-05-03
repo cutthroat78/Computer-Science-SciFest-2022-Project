8e69bfeb8b8d4b328c0b7e0f3c0dd18c7da7d142
cutthroat78 <irisharmycat2@gmail.com>
2022-05-03 23:35:06 +0100

    got zigbee2mqtt working

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 # Computer Science SciFest 2022 Project
-
-## To Do:
-- Control zigbee bulb from program
-- Commands:
-  - Control zigbee bulb
-  - Give Weather
-  - Google Search something and read wikipedia or info on that search topic
-  - Display text command should ask you for what text and then take in your input and display it
+## To Do
+- Control zigbee bulb
+- Give Weather
+- Google Search something and read wikipedia or info on that search topic
+- Display text command should ask you for what text and then take in your input and display it
--- setup.sh
+++ setup.sh
@@ -6,3 +6,8 @@ curl -fsSL https://get.docker.com -o get-docker.sh # Get docker install script
 sudo sh get-docker.sh # Install docker using script
 sudo pip3 install docker-compose # Install docker-compose for quick deployment of zigbee stuff
 sudo systemctl enable docker # Have docker start on system start up
+sudo chmod +x *.sh 
+sudo apt-key adv --keyserver hkps://keyserver.ubuntu.com:443 --recv-keys 04EE7237B7D453EC 648ACFD622F3D138 # This command and below 3 command needed to make zigbe2mqtt to work on raspbian buster
+echo "deb http://httpredir.debian.org/debian buster-backports main contrib non-free" | sudo tee -a "/etc/apt/sources.list.d/debian-backports.list"
+sudo apt update
+sudo apt install libseccomp2 -t buster-backports
